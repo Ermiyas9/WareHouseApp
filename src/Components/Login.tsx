7f87470acc972f1f9eb6e52ac06b275c7543cf90
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-01-16 04:02:03 -0500

    Successfully done the front end UI component of user login page

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,49 +1,64 @@
+import React, { useState } from "react";
+import { Helmet } from "react-helmet-async";
 import UserLoginIcon from "../assets/UserLoginIcon.png";
 import EmailIcon from "../assets/EmailIcon.png";
 import PasswordIcon from "../assets/PasswordIcon.png";
+import "../Components/Login.css";
+import "../index.css";
 
-const Login = () => {
+const Login: React.FC = () => {
+  // setting a state variable
+  const [action, setAction] = useState("Login");
+
   return (
-    <div className="container">
-      <div className="header">
-        <div className="text">Login</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        <div className="input">
-          <img src={UserLoginIcon} alt="" />
-          <input type="text" />
+    <>
+      <p className="title">WareHouse Inventory Hub</p>
+      <div className="container">
+        <div className="header">
+          <div className="text">{action}</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <img src={UserLoginIcon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+
+          <div className="input">
+            <img src={EmailIcon} alt="" />
+            <input type="email" placeholder="Email ID" />
+          </div>
+
+          <div className="input">
+            <img src={PasswordIcon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+        <div className="forgotPassword">
+          Forgot Your Password?
+          <span>Click Here!</span>
+        </div>
+
+        <div className="submitContainer">
+          <div
+            className={action === "Login" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("Register Here");
+            }}
+          >
+            Register Here
+          </div>
+          <div
+            className={action === "Register Here" ? "submit gray" : "submit"}
+            onClick={() => {
+              setAction("Login");
+            }}
+          >
+            Login
+          </div>
         </div>
       </div>
-
-      <div className="inputs">
-        <div className="input">
-          <img src={EmailIcon} alt="" />
-          <input type="email" />
-        </div>
-      </div>
-
-      <div className="inputs">
-        <div className="input">
-          <img src={PasswordIcon} alt="" />
-          <input type="password" />
-        </div>
-      </div>
-      <div className="forgotPassword">
-        <span>Click Here!</span>
-      </div>
-
-      <div className="submitContainer">
-        <div className="submit">Register Here</div>
-        <div className="submit">Login</div>
-      </div>
-
-      <div className="forgotPassword">
-        {" "}
-        Lost Password?
-        <span>Click Here!</span>
-      </div>
-    </div>
+    </>
   );
 };
 
